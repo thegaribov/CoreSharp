fab3b313986a12b9d4c04641990f6f5edbc92614
Mahmood Garibov <mahmood.garibov@gmail.com>
2024-05-14 20:26:01 +0400

    Multiple conditioning was completed

--- CoreSharp/Program.cs
+++ CoreSharp/Program.cs
@@ -38,6 +38,40 @@
             //============================================================
 
 
+            //string personName = "Mahmood";
+            //string personLastName = "Garibov";
+            //string personFatherName = "Elchin";
+            //bool isMarried = true;
+            //bool hasChild = false;
+            //int age = 20;
+
+            //Console.WriteLine(personName);
+            //Console.WriteLine(personLastName);
+            //Console.WriteLine(personFatherName);
+
+            ////if condition, selection statement
+            //if (isMarried) //codnition
+            //{
+
+            //    Console.WriteLine("Evlidir");
+            //    if (hasChild)
+            //    {
+            //        Console.WriteLine("Ovladi var");
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("Ovladi yoxdur");
+            //    }
+            //}
+            //else
+            //{   
+            //    Console.WriteLine("Subay");
+            //}
+
+
+            //============================================================
+
+            //input
             string personName = "Mahmood";
             string personLastName = "Garibov";
             string personFatherName = "Elchin";
@@ -49,25 +83,25 @@
             Console.WriteLine(personLastName);
             Console.WriteLine(personFatherName);
 
-            //if condition, selection statement
-            if (isMarried) //codnition
+            if (age <= 18)
             {
-
-                Console.WriteLine("Evlidir");
-                if (hasChild)
-                {
-                    Console.WriteLine("Ovladi var");
-                }
-                else 
-                {
-                    Console.WriteLine("Ovladi yoxdur");
-                }
+                Console.WriteLine("Yeniyetme");
+            }
+            else if (age >= 19 && age <= 30)
+            {
+                Console.WriteLine("Orya yasli");
+            }
+            else if (age > 30 && age <= 65)
+            {
+                Console.WriteLine("Qocaa");
             }
             else
             {
-                
-                Console.WriteLine("Subay");
+                Console.WriteLine("Yasli");
             }
+
+
+          
         }
     }
 }

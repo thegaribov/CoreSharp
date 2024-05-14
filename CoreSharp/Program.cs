69e45b1f03eae83bbd18c748978f37ed3437efcb
Mahmood Garibov <mahmood.garibov@gmail.com>
2024-05-14 19:43:50 +0400

    Basic flow control with if-else was completed

--- CoreSharp/Program.cs
+++ CoreSharp/Program.cs
@@ -4,8 +4,46 @@
     {
         static void Main(string[] args)
         {
+            string personName = "Mahmood";
+            string personLastName = "Garibov";
+            string personFatherName = "Elchin";
+            bool isMarried = false;
 
-            
+            Console.WriteLine(personName);
+            Console.WriteLine(personLastName);
+            Console.WriteLine(personFatherName);
+
+
+            //== Comparison operator
+
+            //if (isMarried == true) //codnition
+            //{
+            //    Console.WriteLine("Evli");
+            //}
+
+            //============================================================
+
+            //if (isMarried) //codnition
+            //{
+            //    Console.WriteLine("Evli");
+            //}
+
+            //if (isMarried == false)
+            //{
+            //    Console.WriteLine("Subay");
+            //}
+
+
+            //============================================================
+
+            if (isMarried) //codnition
+            {
+                Console.WriteLine("Evli");
+            }
+            else
+            {
+                Console.WriteLine("Subay");
+            }
         }
     }
 }

b1d0832bd9f3b74caa372f52d754cac4c25d8fb0
Mahmood Garibov <mahmood.garibov@gmail.com>
2024-05-16 21:08:01 +0400

    Arra data structure implemented to our main applicaiton

--- CoreSharp/Program.cs
+++ CoreSharp/Program.cs
@@ -6,7 +6,21 @@
         {
             decimal resultTotal = 0;
 
-            while (true)
+            int numberOfShoes = 2;
+
+            //database
+            string[] operatorNames = new string[numberOfShoes];
+            int[] sizes = new int[numberOfShoes];
+            string[] colors = new string[numberOfShoes];
+            string[] brandNames = new string[numberOfShoes];
+            string[] categories = new string[numberOfShoes];
+            decimal[] prices = new decimal[numberOfShoes];
+            int[] counts = new int[numberOfShoes];
+
+
+            int idx = 0;
+
+            while (idx < numberOfShoes)
             {
                 Console.WriteLine("=============================================================================================");
                 Console.WriteLine("=============================================================================================");
@@ -22,7 +36,10 @@
                     operatorName = Console.ReadLine();
 
                     if (operatorName.Length >= 3 && operatorName.Length <= 15)
+                    {
+                        operatorNames[idx] = operatorName;
                         break;
+                    }
                     else
                         Console.WriteLine("Size dugun daxil edilmeyib");
                 }
@@ -37,7 +54,10 @@
                     size = int.Parse(Console.ReadLine());
 
                     if (size >= 30 && size <= 50)
+                    {
+                        sizes[idx] = size;
                         break;
+                    }
                     else
                         Console.WriteLine("Size dugun daxil edilmeyib");
                 }
@@ -52,7 +72,10 @@
                     color = Console.ReadLine();
 
                     if (color == "Red" || color == "Black" || color == "Blue")
+                    {
+                        colors[idx] = color;
                         break;
+                    }
                     else
                         Console.WriteLine("Color dugun daxil edilmeyib");
                 }
@@ -61,15 +84,19 @@
 
                 Console.Write("Brand name : ");
                 string brandName = Console.ReadLine();
+                brandNames[idx] = brandName;
 
                 Console.Write("Category : ");
                 string category = Console.ReadLine(); //Short cut to replace all : Ctrl + RR
+                categories[idx] = category;
 
                 Console.Write("Price : ");
                 decimal price = decimal.Parse(Console.ReadLine()); //Short cut to replace all : Ctrl + RR
+                prices[idx] = price;
 
                 Console.Write("Count : ");
                 int count = int.Parse(Console.ReadLine()); //Short cut to replace all : Ctrl + RR
+                counts[idx] = count;
 
                 decimal individualTotal = count * price;
                 resultTotal += individualTotal;
@@ -85,8 +112,9 @@
                 {
                     //break based loop control
                     break;
-
                 }
+
+                idx++;
             }
 
 
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -22,59 +22,81 @@
 
             //Console.WriteLine(numbers[77]);
 
-            int[] numbers = { 2, 3, 4, 5, 10, 23, 54, 67, 87, 98 };
-            int numbersArrLastIdx = numbers.Length - 1;
+            //int[] numbers = { 2, 3, 4, 5, 10, 23, 54, 67, 87, 98 };
+            //int numbersArrLastIdx = numbers.Length - 1;
 
 
-            //version 1
-            int firstVersionArrIdx = 0;
-            while (true)
+            ////version 1
+            //int firstVersionArrIdx = 0;
+            //while (true)
+            //{
+            //    Console.WriteLine(numbers[firstVersionArrIdx]);
+
+            //    if (firstVersionArrIdx == numbersArrLastIdx)
+            //    {
+            //        break;
+            //    }
+
+            //    firstVersionArrIdx++;
+            //}
+            //Console.WriteLine("===================================");
+
+            ////version 2
+            //int secondVersionArrIdx = 0;
+            //while (secondVersionArrIdx <= numbersArrLastIdx)
+            //{
+            //    Console.WriteLine(numbers[secondVersionArrIdx]);
+
+            //    secondVersionArrIdx++;
+            //}
+            //Console.WriteLine("=================================== (In reverse order)");
+
+
+            ////version 3 (In reverse order)
+            //int thirdVersionArrIdx = numbersArrLastIdx;
+            //while (thirdVersionArrIdx >= 0)
+            //{
+            //    Console.WriteLine(numbers[thirdVersionArrIdx]);
+
+            //    thirdVersionArrIdx--;
+            //}
+            //Console.WriteLine("===================================");
+
+
+            ////version 4 
+            //int fourthVersionArrIdx = numbersArrLastIdx;
+            //while (fourthVersionArrIdx >= 0)
+            //{
+            //    Console.WriteLine(numbers[numbersArrLastIdx - fourthVersionArrIdx]);
+
+            //    fourthVersionArrIdx--;
+            //}
+
+            string[] numbers = new string[5];
+            int idxGetCounter = 0;
+
+            while (idxGetCounter < numbers.Length)
             {
-                Console.WriteLine(numbers[firstVersionArrIdx]);
+                numbers[idxGetCounter] = Console.ReadLine();
 
-                if (firstVersionArrIdx == numbersArrLastIdx)
-                {
-                    break;
-                }
-
-                firstVersionArrIdx++;
+                idxGetCounter++;
             }
 
-            Console.WriteLine("===================================");
 
-            //version 2
-            int secondVersionArrIdx = 0;
-            while (secondVersionArrIdx <= numbersArrLastIdx)
+
+
+
+
+
+            int idx = 0;
+
+            while (idx < numbers.Length)
             {
-                Console.WriteLine(numbers[secondVersionArrIdx]);
+                Console.WriteLine(numbers[idx]);
 
-                secondVersionArrIdx++;
+                idx++;
             }
 
-            Console.WriteLine("=================================== (In reverse order)");
-
-
-            //version 3 (In reverse order)
-            int thirdVersionArrIdx = numbersArrLastIdx;
-            while (thirdVersionArrIdx >= 0)
-            {
-                Console.WriteLine(numbers[thirdVersionArrIdx]);
-
-                thirdVersionArrIdx--;
-            }
-
-
-            Console.WriteLine("===================================");
-
-
-            //version 4 
-            int fourthVersionArrIdx = numbersArrLastIdx;
-            while (fourthVersionArrIdx >= 0)
-            {
-                Console.WriteLine(numbers[numbersArrLastIdx - fourthVersionArrIdx]);
-
-                fourthVersionArrIdx--;
-            }
         }
     }
 }
